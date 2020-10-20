cc7dc97d065ede32220e1dcc55664606194f8ee6
rihi <19492038+rihi@users.noreply.github.com>
2020-10-20 02:21:30 +0200

    Add tooltexture definitions for game cs:s

--- src/main/java/info/ata4/bspsrc/modules/texture/tooltextures/ToolTextureSets.java
+++ src/main/java/info/ata4/bspsrc/modules/texture/tooltextures/ToolTextureSets.java
@@ -1,5 +1,7 @@
 package info.ata4.bspsrc.modules.texture.tooltextures;
 
+import info.ata4.bsplib.app.SourceAppID;
+import info.ata4.bspsrc.modules.texture.tooltextures.definitions.CssToolTextureDefinitions;
 import info.ata4.bspsrc.modules.texture.tooltextures.definitions.SourceToolTextureDefinitions;
 
 import java.util.Arrays;
@@ -12,7 +14,13 @@ import java.util.Map;
  */
 public enum ToolTextureSets {
 
-    SOURCE_2013(-1, SourceToolTextureDefinitions.getToolTextureDefinitions());
+    SOURCE_2013(-1, SourceToolTextureDefinitions.getToolTextureDefinitions()),
+    CSS(
+            SourceAppID.COUNTER_STRIKE_SOURCE,
+            SOURCE_2013.builder()
+                    .putToolTextureDefinitions(CssToolTextureDefinitions.getToolTextureDefinitions())
+                    .build()
+    );
 
     private final int appId;
     private final Map<String, ToolTextureDefinition> toolTextureDefinitions;
@@ -22,10 +30,40 @@ public enum ToolTextureSets {
         this.toolTextureDefinitions = Collections.unmodifiableMap(new HashMap<>(toolTextureDefinitions));
     }
 
+    private Builder builder() {
+        return new Builder(toolTextureDefinitions);
+    }
+
     public static Map<String, ToolTextureDefinition> forGame(int appId) {
         return Arrays.stream(values())
                 .filter(toolTextureSets -> toolTextureSets.appId == appId)
                 .findAny()
                 .orElse(SOURCE_2013).toolTextureDefinitions;
+    }
+
+    /**
+     * A Builder class for building a set of tooltexture definition
+     */
+    private static class Builder {
+
+        private final Map<String, ToolTextureDefinition> toolTextureDefinitions = new HashMap<>();
+
+        public Builder(Map<String, ToolTextureDefinition> toolTextureDefinitions) {
+            this.toolTextureDefinitions.putAll(toolTextureDefinitions);
+        }
+
+        public Builder putToolTextureDefinitions(Map<String, ToolTextureDefinition> toolTextureDefinitions) {
+            this.toolTextureDefinitions.putAll(toolTextureDefinitions);
+            return this;
+        }
+
+        public Map<String, ToolTextureDefinition> build() {
+            return new HashMap<>(toolTextureDefinitions);
+        }
+
+        public Builder removeToolTextureDefinitions(String... materialNames) {
+            toolTextureDefinitions.keySet().removeAll(Arrays.asList(materialNames));
+            return this;
+        }
     }
 }
